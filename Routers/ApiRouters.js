a7255cf88800d18b31a803e8e33b5ae8fe429414
khalid chouhan <khalidchuhan7886@gmail.com>
2024-12-02 18:32:33 +0500

    comments

--- Routers/ApiRouters.js
+++ Routers/ApiRouters.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { posts } from "../ApiData/Post.js";
+import { comments } from "../ApiData/comments.js";
 
 const ApiRouters = express.Router();
 ApiRouters.get("/", (req, res) => {
   res.status(200).send({ status: 200, message: "Scuess", data: posts });
 });
 
+ApiRouters.get("/comments",(req, res) => {
+  res.status(200).send({ status: 200, message: "Scuess", data: comments });
+})
 export default ApiRouters;
