261d2aea8aea34713522dec2dc07969ee3ccf717
khalid chouhan <khalidchuhan7886@gmail.com>
2024-12-02 19:00:14 +0500

    allchange

--- Routers/ApiRouters.js
+++ Routers/ApiRouters.js
@@ -1,13 +1,27 @@
 import express from "express";
 import { posts } from "../ApiData/Post.js";
 import { comments } from "../ApiData/comments.js";
+import { album } from "../ApiData/album.js";
+import { photo } from "../ApiData/photo.js";
 
 const ApiRouters = express.Router();
-ApiRouters.get("/", (req, res) => {
+ApiRouters.get("/posts", (req, res) => {
   res.status(200).send({ status: 200, message: "Scuess", data: posts });
 });
 
-ApiRouters.get("/comments",(req, res) => {
+ApiRouters.get("/comments", (req, res) => {
   res.status(200).send({ status: 200, message: "Scuess", data: comments });
-})
+});
+ApiRouters.get("/album", (req, res) => {
+  res.status(200).send({ status: 200, message: "Scuess", data: album });
+});
+ApiRouters.get("/photo", (req, res) => {
+  res.status(200).send({ status: 200, message: "Scuess", data: photo });
+});
+ApiRouters.get("/todos", (req, res) => {
+  res.status(200).send({ status: 200, message: "Scuess", data: todos });
+});
+ApiRouters.get("/user", (req, res) => {
+  res.status(200).send({ status: 200, message: "Scuess", data: user });
+});
 export default ApiRouters;
--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import ApiRouters from "./Routers/ApiRouters.js";
-
+import cors from "cors";
 const app = express();
 dotenv.config();
+app.use(cors())
 const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.status(200).send("Welcome to JsonApi");
-});
-app.use("/posts", ApiRouters);
+app.use("/", ApiRouters);
+
+
 app.use((req, res) => {
   res.status(404).send("404 Page Not Found");
 });
+
 app.listen(PORT, () => {
   console.log(`Server Running at http://localhost:${PORT}`);
 });
